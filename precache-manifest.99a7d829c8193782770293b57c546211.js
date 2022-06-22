bc8a844e2ec629f3145e47ffe66ca7d31d3105a1
wmk5055 <kraynik@Wills-Air.hsd1.de.comcast.net>
2022-06-21 21:12:08 -0400

    Updates

---
+++ precache-manifest.99a7d829c8193782770293b57c546211.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c7beb0720230776fa0d",
+    "url": "/static/css/main.d669e5d9.chunk.css"
+  },
+  {
+    "revision": "4c7beb0720230776fa0d",
+    "url": "/static/js/main.068a5397.chunk.js"
+  },
+  {
+    "revision": "31b6faa5ac5540836e4f",
+    "url": "/static/js/runtime~main.bb5fe24d.js"
+  },
+  {
+    "revision": "bdaa743eb0f5fbd20300",
+    "url": "/static/css/2.6df1af00.chunk.css"
+  },
+  {
+    "revision": "bdaa743eb0f5fbd20300",
+    "url": "/static/js/2.dc7d04d3.chunk.js"
+  },
+  {
+    "revision": "79be80b375553eb66abb",
+    "url": "/static/js/3.c70b6b5b.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ac0a8a26cfd8b47311ef599ba91e7fda",
+    "url": "/index.html"
+  }
+];
